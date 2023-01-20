e0750d9cdbea4379c139b2cda12e48fbb9b4930c
MSierski <92363171+MSierski@users.noreply.github.com>
2023-01-20 11:24:39 +0100

    Add files via upload

---
+++ MSier_mapreduce/ratingcount.py
@@ -0,0 +1,31 @@
+from mrjob.job import MRJob
+
+class MRHotelRaitingCount(MRJob):
+    # def mapper(self, _, line):
+    #     (HName, HStar, HRooms, UCountry, NrReviews, rating, StayPeriod, TType, Pool, Gym, TCourt, Spa, Casino, Internet, UContinent, ReviewMonth, ReviewDay) = line.split("\t")
+    #     result = [rating, 1]
+    #     yield result
+
+    # def reducer(self, key, value):
+    #     result = [key, sum(value)]
+    #     yield result
+
+
+     def mapper(self, _, line):
+         (userId, movieId, rating, timestamp) = line.split(",")
+         if (rating != "rating"):    
+            result = [movieId, float(rating)]      
+            yield result
+
+     def reducer(self, key, value):
+        x = 0
+        y = 0
+        for v in value:
+            x += v
+            y += 1
+            result = [key, x / y]
+            yield result
+
+
+if __name__ == '__main__':
+    MRHotelRaitingCount.run()
